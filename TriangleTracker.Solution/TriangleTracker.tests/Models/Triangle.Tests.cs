0b128660d5bc8de4e04ce79ba6c8d56e9ce1aa5d
Ian Gregg <iangregg188@gmail.com>
2020-07-14 14:52:31 -0700

    Isosceles is passing the red green refactoring test. > > Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

--- TriangleTracker.Solution/TriangleTracker.tests/Models/Triangle.Tests.cs
+++ TriangleTracker.Solution/TriangleTracker.tests/Models/Triangle.Tests.cs
@@ -13,15 +13,20 @@ namespace TriangleTracker.Tests
       Triangle testTriangle = new Triangle(3, 3, 3);
       Assert.AreEqual(true, testTriangle.isEquilateral());
     }
+
+    [TestMethod]
     public void IsIsosceles_TwoSidesAreEqual_True()
     {
       Triangle testTriangle = new Triangle(3, 2, 3);
-      Assert.AreEqual(true, testTriangle.isEquilateral());
+      Assert.AreEqual(true, testTriangle.isIsosceles());
     }
-    // public void IsScalene_NoSidesAreEqual_True()
-    // {
-    //   Triangle testTriangle = new Triangle();
-    // }
+    
+    [TestMethod]
+    public void IsScalene_NoSidesAreEqual_True()
+    {
+      Triangle testTriangle = new Triangle(3,1,2);
+      Assert.AreEqual(true, testTriangle.isScalene());
+    }
   }
 }
 

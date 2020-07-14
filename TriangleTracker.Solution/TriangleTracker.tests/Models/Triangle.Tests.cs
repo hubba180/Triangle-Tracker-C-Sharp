9508601435c4f30d1649ea5efbd76032fd476cb7
Ian Gregg <iangregg188@gmail.com>
2020-07-14 11:12:18 -0700

    ReEstablishing the web page because pulled in the wrong info.
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

---
+++ TriangleTracker.Solution/TriangleTracker.tests/Models/Triangle.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleTracker;
+
+
+namespace TriangleTracker.Tests
+{
+  [TestClass]
+  public class TriangleTests
+  {
+    [TestMethod]
+    // testing method here
+  }
+}
---
+++ TriangleTracker.Solution/TriangleTracker/Models/Triangle.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TriangleTracker 
+{
+  public class Triangle
+  {
+
+  }
+}
